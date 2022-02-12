4f166d652a0e2702c8b9bb1fb655b0fc307e3837
yodareushi <hugahogehige@gmail.com>
2022-02-12 22:49:55 +0900

    test　commit

---
+++ test.py
@@ -0,0 +1,8 @@
+import re
+
+a = 2
+print("aaa")
+
+if (a == 2):
+    a = +1
+b = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
